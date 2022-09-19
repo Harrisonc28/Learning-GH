54773c3ca9e212b513ecd27d00a43313d2916450
Harrison Carroll <harrison_2000@live.co.uk>
2022-09-19 19:02:57 +0100

    Initial Commit

---
+++ Shared/LearningGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGBApp.swift
+//  Shared
+//
+//  Created by Harrison Carroll on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
